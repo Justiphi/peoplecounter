1131703221f6580b4644e966caaadae1de2622d3
Travis Wylie <traviswylie58@gmail.com>
2019-04-24 07:56:48 +1200

    initial commit

---
+++ FaceCounter/ImageProcessor.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.Drawing;
+using System.Diagnostics;
+using Emgu.CV.Dpm;
+
+namespace FaceCounter
+{
+    public static class ImageProcessor
+    {
+        /// <summary>
+        /// Find the pedestrian in the image
+        /// </summary>
+        /// <param name="image">The image</param>
+        /// <param name="processingTime">The pedestrian detection time in milliseconds</param>
+        /// <returns>The image with pedestrian highlighted.</returns>
+        public static Image<Bgr, Byte> Find(Image<Bgr, Byte> image, out long processingTime, out int count)
+        {
+            Stopwatch watch;
+            //MCvObjectDetection[] regions2;
+            Rectangle[] regions;
+
+            ////check if there is a compatible GPU to run pedestrian detection
+            //if (DpmInvoke.HasCuda)
+            //{  //this is the GPU version
+            //    using (GpuHOGDescriptor des = new GpuHOGDescriptor())
+            //    {
+            //        des.SetSVMDetector(GpuHOGDescriptor.GetDefaultPeopleDetector());
+
+            //        watch = Stopwatch.StartNew();
+            //        using (GpuImage<Bgr, Byte> gpuImg = new GpuImage<Bgr, byte>(image))
+            //        using (GpuImage<Bgra, Byte> gpuBgra = gpuImg.Convert<Bgra, Byte>())
+            //        {
+            //            regions = des.DetectMultiScale(gpuBgra);
+            //        }
+            //    }
+            //}
+            //else
+            //{  //this is the CPU version
+            //using (HOGDescriptor des = new HOGDescriptor())
+            //{
+            //    des.SetSVMDetector(HOGDescriptor.GetDefaultPeopleDetector());
+
+            //    watch = Stopwatch.StartNew();
+            //    regions2 = des.DetectMultiScale(image);
+            //}
+            //}
+            using (CascadeClassifier des = new CascadeClassifier("C:\\Emgu\\emgucv-windesktop 3.4.3.3016\\etc\\haarcascades\\haarcascade_frontalface_default.xml"))
+            //using (CascadeClassifier des = new CascadeClassifier("C:\\Emgu\\emgucv-windesktop 3.4.3.3016\\etc\\haarcascades\\haarcascade_frontalface_alt.xml"))
+            //using (CascadeClassifier des = new CascadeClassifier("C:\\Emgu\\emgucv-windesktop 3.4.3.3016\\etc\\haarcascades\\haarcascade_fullbody.xml"))
+            //using (CascadeClassifier des = new CascadeClassifier("C:\\Users\\Travis\\Desktop\\HS.xml"))
+            {
+                var imageClone = image.Convert<Gray, byte>().Clone();
+                watch = Stopwatch.StartNew();
+                regions = des.DetectMultiScale(imageClone, 1.1, 3);
+            }
+
+            watch.Stop();
+
+            processingTime = watch.ElapsedMilliseconds;
+            count = regions.Length;
+
+            //foreach (MCvObjectDetection pedestrain in regions2)
+            //{
+            //    image.Draw(pedestrain.Rect, new Bgr(Color.Blue), 5);
+            //}
+            foreach (Rectangle pedestrain in regions)
+            {
+                image.Draw(pedestrain, new Bgr(Color.Red), 5);
+            }
+            return image;
+        }
+    }
+}
---
+++ FaceCounter/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace FaceCounter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            long responseTime;
+            int count;
+            //Mat mat = CvInvoke.Imread("C:\\Users\\Travis\\source\\repos\\FaceCounter\\FaceCounter\\camera4.jpg", Emgu.CV.CvEnum.ImreadModes.AnyColor);
+            Mat mat = CvInvoke.Imread("C:\\Users\\Travis\\source\\repos\\FaceCounter\\FaceCounter\\testImage.jpeg", Emgu.CV.CvEnum.ImreadModes.AnyColor);
+            //Mat mat = CvInvoke.Imread("C:\\Users\\Travis\\source\\repos\\FaceCounter\\FaceCounter\\testing.jpg", Emgu.CV.CvEnum.ImreadModes.AnyColor);
+            Image<Bgr, Byte> img = mat.ToImage<Bgr, Byte>();
+            Image<Bgr, Byte> response = ImageProcessor.Find(img, out responseTime, out count);
+            BitmapSource responseSource = ToBitmapSource(response);
+            itemLabel.Content = $"{count} objects found in {responseTime}ms";
+            imageBox.Source = responseSource;
+            //showPicture(response);
+        }
+
+        private void showPicture(Image<Bgr, Byte> image)
+        {
+            ImageViewer.Show(image, "Tester");
+        }
+
+
+        /// <summary>
+        /// Delete a GDI object
+        /// </summary>
+        /// <param name="o">The poniter to the GDI object to be deleted</param>
+        /// <returns></returns>
+        [DllImport("gdi32")]
+        private static extern int DeleteObject(IntPtr o);
+
+        /// <summary>
+        /// Convert an IImage to a WPF BitmapSource. The result can be used in the Set Property of Image.Source
+        /// </summary>
+        /// <param name="image">The Emgu CV Image</param>
+        /// <returns>The equivalent BitmapSource</returns>
+        public static BitmapSource ToBitmapSource(IImage image)
+        {
+            using (System.Drawing.Bitmap source = image.Bitmap)
+            {
+                IntPtr ptr = source.GetHbitmap(); //obtain the Hbitmap
+
+                BitmapSource bs = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                    ptr,
+                    IntPtr.Zero,
+                    Int32Rect.Empty,
+                    System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+
+                DeleteObject(ptr); //release the HBitmap
+                return bs;
+            }
+        }
+    }
+}
